afb0812b903d4566c4fc63d5edb026202f312a68
Alireza Dehghanzadeh <86428901+alerezaaa@users.noreply.github.com>
2023-09-13 19:11:47 +0330

    Update setup.py

--- namizun_core/setup.py
+++ namizun_core/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='namizun_core',
-      version='1.3.8',
+      version='1.3.8.1',
       description='namizun main functions',
       author='MalKeMit',
       author_email='khodemalkemit@gmail.com',
